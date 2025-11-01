eed427ed0cc608ac76e8add45fc78c44904ed63f
Kelvin Gautama <kelvinpersib99@gmail.com>
2025-11-01 16:04:37 +0000

    masih sama

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  basePath: '/carton-sheet-calc',
   typescript: {
     ignoreBuildErrors: true,
   },
